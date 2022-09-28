fdb4c6274e65942e6f9196670c754b2a00df64d2
Student9876 <shouvik9876@gmail.com>
2022-09-28 11:58:15 +0530

    Variables in Javascript

---
+++ 001_add_3_numbers.js
@@ -0,0 +1,8 @@
+console.log("This programe adds three numbers")
+
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+
+let c = addThreeNumbers(2,3,6)
+console.log(c)
---
+++ 002_variables.js
@@ -0,0 +1,7 @@
+// Variables in javascript can change its type in runtime 
+console.log("This contains code for the Variable in Javascript ")
+var a = 67 // we will not use this 
+let b = 67;
+console.log(b);
+b = "Arch Linux";
+console.log(b);
