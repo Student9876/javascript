9fb88e45f8cebf0ae87130481cec0c2ff22a636a
Student9876 <shouvik9876@gmail.com>
2022-09-29 10:03:38 +0530

    Javascript PDF Notes from CodeWithHarry Added

---
+++ Code/003_var_let_const.js
@@ -0,0 +1,19 @@
+console.log("Javascript Tutorial: var, let and constant")
+var a = 45;
+var b = "Zeon";
+var c = null;
+var d = undefined
+const author = "ZEON"
+// author = "D" // Thorws an error as it is declared as a const
+const value;
+// var can be defined multiple times. So it may cause error in future.
+// Making a block
+{
+    // var b = "this";
+    // if we use let then the operation is in the block. Variable is not changed globally
+    let b = "this";
+    console.log(b)
+}
+console.log(b)
+
+// Better use let instead of var 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript
+
+PDF Notes from CodeWithHarry
