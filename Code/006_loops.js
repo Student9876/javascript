84cd4f9f1aa1d534fc4a0918c97962fc570bd1e7
Student9876 <shouvik9876@gmail.com>
2022-10-25 13:05:44 +0530

    Loops in Js

---
+++ Code/006_loops.js
@@ -0,0 +1,12 @@
+console.log("Loops in Js");
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// Program to add first n natural number
+let sum = 0;
+let n = prompt("Enter value of n: ");
+n = Number.parseInt(n);
+for (let j = 0; j < n; j++) {
+    console.log(j + 1);
+}
